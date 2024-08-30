8a4dfca78159ea09d951c11a5d54e0329fdd5610
ntsh-oni <teamnutshell.dev@gmail.com>
2024-08-30 14:48:06 +0200

    add and change some global resources

--- src/widgets/project_menu.cpp
+++ src/widgets/project_menu.cpp
@@ -1,5 +1,6 @@
 #include "project_menu.h"
 #include "project_ntpj_file_widget.h"
+#include <fstream>
 
 ProjectMenu::ProjectMenu(GlobalInfo& globalInfo) : QMenu("&Project"), m_globalInfo(globalInfo) {
 	m_openProjectSettingsAction = addAction("Open Project Settings", this, &ProjectMenu::openProjectSettings);
@@ -16,6 +17,27 @@ void ProjectMenu::openProjectSettings() {
 
 void ProjectMenu::importGlobalResources() {
 	std::filesystem::copy("assets/global_resources", m_globalInfo.projectDirectory, std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive);
+
+	std::fstream optionsFile(m_globalInfo.projectDirectory + "/assets/options/options.ntop", std::ios::in);
+	if (optionsFile.is_open()) {
+		if (!nlohmann::json::accept(optionsFile)) {
+			return;
+		}
+	}
+	else {
+		return;
+	}
+
+	optionsFile = std::fstream(m_globalInfo.projectDirectory + "/assets/options/options.ntop", std::ios::in);
+	nlohmann::json j = nlohmann::json::parse(optionsFile);
+
+	optionsFile = std::fstream(m_globalInfo.projectDirectory + "/assets/options/options.ntop", std::ios::out | std::ios::trunc);
+	if (optionsFile.is_open()) {
+		if (!j.contains("firstScenePath")) {
+			j["firstScenePath"] = "assets/scenes/default_scene.ntsn";
+		}
+		optionsFile << j.dump(1, '\t');
+	}
 }
 
 void ProjectMenu::updateBaseProject() {
