0c12c03790eedabf4411c1f319acc7fc398fdb75
ntsh-oni <teamnutshell.dev@gmail.com>
2025-01-29 01:04:26 +0100

    add screen-door transparency to renderer

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -112,6 +112,13 @@ void Renderer::initializeGL() {
 	std::string entityFragmentShaderCode = R"GLSL(
 	#version 460
 
+	const mat4 ditheringThreshold = mat4(
+		1.0 / 17.0, 13.0 / 17.0, 4.0 / 17.0, 16.0 / 17.0,
+		9.0 / 17.0, 5.0 / 17.0, 12.0 / 17.0, 8.0 / 17.0,
+		3.0 / 17.0, 15.0 / 17.0, 2.0 / 17.0, 14.0 / 17.0,
+		11.0 / 17.0, 7.0 / 17.0, 10.0 / 17.0, 6.0 / 17.0
+	);
+
 	struct Light {
 		vec3 position;
 		vec3 direction;
@@ -137,7 +144,7 @@ void Renderer::initializeGL() {
 
 	void main() {
 		vec4 diffuseTextureSample = texture(diffuseTextureSampler, fragUV);
-		if (diffuseTextureSample.a < alphaCutoff) {
+		if ((diffuseTextureSample.a < alphaCutoff) || (diffuseTextureSample.a < ditheringThreshold[int(mod(gl_FragCoord.x, 4.0))][int(mod(gl_FragCoord.y, 4.0))])) {
 			discard;
 		}
 		vec3 emissiveTextureSample = texture(emissiveTextureSampler, fragUV).rgb;
