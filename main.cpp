96f10e71e94c5cc498ce8079a218e30a898d0e6c
ntsh-oni <teamnutshell.dev@gmail.com>
2025-05-26 17:51:05 +0200

    version 0.1.14

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 	application->setStyle("Fusion");
 
 	GlobalInfo globalInfo;
-	globalInfo.version = "0.1.13";
+	globalInfo.version = "0.1.14";
 	globalInfo.devicePixelRatio = application->devicePixelRatio();
 	globalInfo.actionUndoStack = std::make_unique<QUndoStack>();
 	globalInfo.selectionUndoStack = std::make_unique<QUndoStack>();
