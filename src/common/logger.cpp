195f4d5962e5ca9e17a0652116137de01c3b7bfc
ntsh-oni <teamnutshell.dev@gmail.com>
2025-07-12 14:23:13 +0200

    minor format fix

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -6,7 +6,6 @@ Logger::Logger() {
 
 void Logger::addLog(LogLevel logLevel, const std::string& logMessage) {
 	 m_logs.push_back({ std::time(nullptr), logLevel, logMessage });
-
 	 emit addLogSignal();
 }
 
