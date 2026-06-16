fbc517d468130a75196fac0ffecc7b7fdfb43316
ntsh-oni <teamnutshell.dev@gmail.com>
2026-06-16 14:39:03 +0200

    add a separator in options menu

--- src/widgets/options_menu.cpp
+++ src/widgets/options_menu.cpp
@@ -4,6 +4,7 @@
 
 OptionsMenu::OptionsMenu(GlobalInfo& globalInfo) : QMenu("&" + QString::fromStdString(globalInfo.localization.getString("header_options"))), m_globalInfo(globalInfo) {
 	m_openEditorParametersAction = addAction(QString::fromStdString(m_globalInfo.localization.getString("header_options_open_editor_parameters")), this, &OptionsMenu::openEditorParameters);
+	addSeparator();
 	m_openProjectOptionsAction = addAction(QString::fromStdString(m_globalInfo.localization.getString("header_options_open_project_options")), this, &OptionsMenu::openProjectOptions);
 }
 
